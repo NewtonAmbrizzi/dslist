d9763359dde8cc0c836e1c5cae9eb8886c079e2c
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-05-11 01:12:20 -0300

    Get gamebyid

--- src/main/java/com/newton/dslist/services/GameService.java
+++ src/main/java/com/newton/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.newton.dslist.dto.GameDTO;
 import com.newton.dslist.dto.GameMinDTO;
 import com.newton.dslist.entities.Game;
 import com.newton.dslist.repositories.GameRepository;
@@ -15,9 +17,16 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> gameList = gameRepository.findAll();
         return gameList.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
     
 }
